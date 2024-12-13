a053b011933b6c90901a67db06650246821ecb4d
Vexxys Development <122513007+VexxyDevs@users.noreply.github.com>
2024-12-13 09:47:32 +0000

    Create custom_Camera.h

---
+++ custom_Camera.h
@@ -0,0 +1,15 @@
+_SCamera get_view_point()
+{
+	_SCamera view_point{};
+	uintptr_t loc = Kernel.Read<uintptr_t>(cached::gworld + 0x130); //
+	uintptr_t rott = Kernel.Read<uintptr_t>(cached::gworld + 0x140); //
+	FNRot fnrot{};
+	fnrot.a = Kernel.Read<double>(rott);
+	fnrot.b = Kernel.Read<double>(rott + 0x20);
+	fnrot.c = Kernel.Read<double>(rott + 0x1D0);
+	view_point.location = read<Vector3>(loc);
+	view_point.rotation.x = asin(fnrot.c) * (180.0 / M_PI);
+	view_point.rotation.y = ((atan2(fnrot.a * -1, fnrot.b) * (180.0 / M_PI)) * -1) * -1;
+	view_point.fov = Kernel.Read<float>(cached::player_controller + 0x3AC) * 90.0f;
+	return view_point;
+}
