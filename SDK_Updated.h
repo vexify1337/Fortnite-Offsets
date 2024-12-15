4626d24de67ddb88b501d38b690b44766e968f67
Lowkey Ud <122513007+VexxyDevs@users.noreply.github.com>
2024-12-15 15:30:40 +0000

    Create SDK_Updated.h

---
+++ SDK_Updated.h
@@ -0,0 +1,15 @@
+namespace offsets {
+ 	inline static uintptr_t LastRenderTime = 0x30C;
+
+}
+
+namespace fnsdk {
+bool VisiCheck(uintptr_t cached_mesh)
+	{
+		auto Mili = Kernel.Read<double>(cached::gworld + 0x150);
+		auto LastRenderTime = Kernel.Read<float>(cached_mesh + offsets::LastRenderTime); // 0x 0x30C
+		return Mili - LastRenderTime <= 0.06f;
+	}
+}
+
+// ill add more cheat functions like name esp, weapon update to this soon
