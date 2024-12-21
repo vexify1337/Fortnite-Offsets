482a7494ce4f2a6af34558da1be6c2785d6e27c5
Lowkey Ud <122513007+vexify1337@users.noreply.github.com>
2024-12-21 11:34:16 +0000

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -7,7 +7,7 @@ _SCamera get_view_point()
 	fnrot.a = Kernel.Read<double>(rott);
 	fnrot.b = Kernel.Read<double>(rott + 0x20);
 	fnrot.c = Kernel.Read<double>(rott + 0x1D0);
-	view_point.location = read<Vector3>(loc);
+	view_point.location = Kernel.Read<Vector3>(loc);
 	view_point.rotation.x = asin(fnrot.c) * (180.0 / M_PI);
 	view_point.rotation.y = ((atan2(fnrot.a * -1, fnrot.b) * (180.0 / M_PI)) * -1) * -1;
 	view_point.fov = Kernel.Read<float>(cached::player_controller + 0x3AC) * 90.0f;
