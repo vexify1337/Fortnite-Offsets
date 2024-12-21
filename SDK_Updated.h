b1481399a19021f4935c6234fa60195005353f6d
Lowkey Ud <122513007+vexify1337@users.noreply.github.com>
2024-12-21 11:35:07 +0000

    Update SDK_Updated.h

--- SDK_Updated.h
+++ SDK_Updated.h
@@ -1,12 +1,13 @@
 namespace offsets {
  	inline static uintptr_t LastRenderTime = 0x30C;
+inline static uintptr_t WorldSeconds = 0x148;
 
 }
 
 namespace fnsdk {
 bool VisiCheck(uintptr_t cached_mesh)
 	{
-		auto Mili = Kernel.Read<double>(cached::gworld + 0x150);
+		auto Mili = Kernel.Read<double>(cached::gworld + offsets::WorldSeconds);
 		auto LastRenderTime = Kernel.Read<float>(cached_mesh + offsets::LastRenderTime); // 0x30C
 		return Mili - LastRenderTime <= 0.06f;
 	}
